f48abe0e310e5ec376826027841dd9bb7c186b62
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-08 15:22:20 -0400

    fix navbar for mobile view

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,24 @@
-import { Locale } from "next-intl";
+import { useLocale } from "next-intl";
 import MamanpazIcon from "../../public/MamanpazIcon.svg";
 import MamanpazWordmark from "../../public/MamanpazWordmark.svg";
 import LocaleSwitcher from "./locale-switcher";
 import { MaxWidthWrapper } from "./max-width-wrapper";
-import { setRequestLocale } from "next-intl/server";
+import { cn } from "@/lib/utils";
 
 export const Navbar = () => {
     // setRequestLocale(locale)
+    const locale = useLocale()
   return (
-    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full bg-brand-navy/80 backdrop-blur-lg transition-all md:h-20">
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full bg-brand-navy/90 backdrop-blur-lg transition-all md:h-20">
       <MaxWidthWrapper>
-        <div className="flex h-full my-auto items-center justify-between">
-          <div className="h-full w-fit flex gap-1 items-center">
-            <MamanpazIcon className="h-10 text-white md:h-12" />
-            <MamanpazWordmark className="h-10 text-white md:h-12" />
+        <div className={cn("flex h-16 items-center justify-between",
+          locale === 'fa' ? ' flex-row-reverse' : 'flex-row'
+        )}>
+          <div className={cn("h-full w-fit flex gap-1 items-center py-2", 
+            locale === 'fa' ? ' flex-row-reverse' : 'flex-row'
+          )}>
+            <MamanpazIcon className="h-full p-1 text-white md:h-12" />
+            <MamanpazWordmark className="h-full text-white hidden md:h-12" />
           </div>
           <LocaleSwitcher />
         </div>
