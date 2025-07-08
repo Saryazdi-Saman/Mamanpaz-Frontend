3b274df03d1c09208ebc7d7a7a1c7c08d59f3220
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-07 21:25:50 -0400

    add umami tracker

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,37 +4,36 @@ import { notFound } from "next/navigation";
 import "./../globals.css";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
-import {Vazirmatn, STIX_Two_Text, Lato} from 'next/font/google'
-import localFont from 'next/font/local'
+import { Vazirmatn, STIX_Two_Text, Lato } from "next/font/google";
+import localFont from "next/font/local";
 import { Metadata } from "next";
+import Script from "next/script";
 
 const stixTwoText = STIX_Two_Text({
-  subsets: ['latin'],
+  subsets: ["latin"],
   variable: "--font-stix-two-text",
-  display: "swap"
-})
+  display: "swap",
+});
 
 const lato = Lato({
-  subsets: ['latin'],
-  style: ['italic', 'normal'],
-  weight: ['100', '400', '700'],
+  subsets: ["latin"],
+  style: ["italic", "normal"],
+  weight: ["100", "400", "700"],
   variable: "--font-lato",
-  display: "swap"
-})
+  display: "swap",
+});
 
 const decoTypeThuluth = localFont({
-  src: '../../../public/decotype_thuluth_regular.woff2',
-  variable: '--font-decotype-thuluth',
-  display: 'swap'
-})
+  src: "../../../public/decotype_thuluth_regular.woff2",
+  variable: "--font-decotype-thuluth",
+  display: "swap",
+});
 
 const vazirmatn = Vazirmatn({
-  subsets: ['arabic'],
-  variable: '--font-vazirmatn',
-  display: 'swap',
-})
-
-
+  subsets: ["arabic"],
+  variable: "--font-vazirmatn",
+  display: "swap",
+});
 
 type Props = {
   children: React.ReactNode;
@@ -45,18 +44,19 @@ export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
 
-export async function generateMetadata(props: Omit<Props, "children">):Promise<Metadata> {
+export async function generateMetadata(
+  props: Omit<Props, "children">
+): Promise<Metadata> {
   const { locale } = await props.params;
 
   const t = await getTranslations({ locale, namespace: "LocaleLayout" });
 
   return {
     title: t("title"),
-    applicationName: 'Mamanpaz',
+    applicationName: "Mamanpaz",
     appleWebApp: {
-      title: 'Mamanpaz'
+      title: "Mamanpaz",
     },
-
   };
 }
 
@@ -73,17 +73,29 @@ export default async function LocaleLayout({ children, params }: Props) {
   // Determine font classes based on locale
   // const fontClasses =  `${decoTypeThuluth.variable} ${vazirmatn.variable} ${stixTwoText.variable} ${lato.variable}`
 
-  const bodyFont = locale === "fa" ? 'font-vazir' : 'font-lato'
-  const fontClasses = locale === "fa" 
-    ? `${decoTypeThuluth.variable} ${vazirmatn.variable}`
-    : `${stixTwoText.variable} ${lato.variable} ${decoTypeThuluth}`;
-
+  const bodyFont = locale === "fa" ? "font-vazir" : "font-lato";
+  const fontClasses =
+    locale === "fa"
+      ? `${decoTypeThuluth.variable} ${vazirmatn.variable}`
+      : `${stixTwoText.variable} ${lato.variable} ${decoTypeThuluth}`;
 
   return (
-    <html className="h-full" lang={locale} dir={locale === "fa" ? "rtl" : "ltr"}>
-      <body className={`${fontClasses} ${bodyFont} flex h-full flex-col antialiased`}>
+    <html
+      className="h-full"
+      lang={locale}
+      dir={locale === "fa" ? "rtl" : "ltr"}
+    >
+      <body
+        className={`${fontClasses} ${bodyFont} flex h-full flex-col antialiased`}
+      >
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
+      {/* Umami tracking script */}
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="4022d3ad-820b-4a0f-b8f4-53d5ae81107a"
+      />
     </html>
   );
 }
