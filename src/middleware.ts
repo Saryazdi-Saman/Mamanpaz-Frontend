454e60a3f88cc75b8ba2428bbdc7a82510d34618
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-08 18:37:47 -0400

    clean middleware file to prepare for extentions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+import type { NextRequest } from "next/server";
+
+const i18nMiddleware = createMiddleware(routing)
+
+export function middleware(req: NextRequest) {
+  const res = i18nMiddleware(req);
+  console.log(res)
+  return res
+}
+
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
