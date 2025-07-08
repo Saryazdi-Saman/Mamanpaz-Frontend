8e046475e32936f8f554ece7f8069d6253a138a1
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-08 14:38:54 -0400

    add wide tracking to root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -86,7 +86,7 @@ export default async function LocaleLayout({ children, params }: Props) {
       dir={locale === "fa" ? "rtl" : "ltr"}
     >
       <body
-        className={`${fontClasses} ${bodyFont} flex h-full w-screen flex-col antialiased`}
+        className={`${fontClasses} ${bodyFont} flex h-full w-screen flex-col antialiased tracking-wide`}
       >
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
