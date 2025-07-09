03616eba5ef795d133a80870c31cae625466b66e
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-08 23:28:35 -0400

    fixed layout

---
+++ src/app/manifest.ts
@@ -0,0 +1,38 @@
+import {MetadataRoute} from 'next';
+import {getTranslations} from 'next-intl/server';
+import {routing} from '@/i18n/routing';
+
+export default async function manifest(): Promise<MetadataRoute.Manifest> {
+  const t = await getTranslations({
+    locale: routing.defaultLocale,
+    namespace: 'Manifest'
+  });
+
+  return {
+    name: t('name'),
+    short_name: 'MamPaz',
+    description: 'Mamanpaz - Your favorite meal delivery service',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1D214E',
+    theme_color: '#1D214E',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: '32x32',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/icon1.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+    ],
+  };
+}
