4f3aa92cd759dabb49f8312f14dffb361fbce002
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-10 23:53:26 -0400

    add image backgroung

--- src/app/[locale]/(landing)/coming-soon/v1/page.tsx
+++ src/app/[locale]/(landing)/coming-soon/v1/page.tsx
@@ -46,7 +46,7 @@ export default async function V1({
             alt="Sketch of a mom handing a plate of zereshk polo ba morgh to her child."
             width={1024}
             height={1536}
-            className="h-72 w-auto max-h-80 md:w-2/5 md:h-auto md:max-h-none landscape:min-h-48 z-10"
+            className="h-72 w-auto max-h-80 md:w-2/5 md:h-auto md:max-h-none landscape:min-h-48 z-10 bg-brand-navy"
           />
         </div>
       </MaxWidthWrapper>
