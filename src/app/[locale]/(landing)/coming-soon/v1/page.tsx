40d8578dc7b87c5ebe64e0fc7b82db1db3075710
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-10 21:45:26 -0400

    change hero image

--- src/app/[locale]/(landing)/coming-soon/v1/page.tsx
+++ src/app/[locale]/(landing)/coming-soon/v1/page.tsx
@@ -29,26 +29,25 @@ export default async function V1({
       <MaxWidthWrapper className="text-white min-h-[calc(100vh-4rem)] flex items-start md:items-center">
         <div
           data-type="hero-layout"
-          className=" w-full flex flex-col-reverse items-center py-4 px-4 justify-start
-          md:flex-row md:gap-16 md:justify-between
+          className=" w-full flex flex-col-reverse items-center px-4 justify-start
+          md:flex-row md:gap-16 md:justify-between md:py-4
           landscape:py-4 landscape:gap-4"
         >
           <div
             data-type="copy-box"
-            className="w-full h-fit space-y-8 bg-brand-navy/10 backdrop-blur-sm py-2
+            className="w-full h-fit space-y-8 bg-brand-navy/10 backdrop-blur-sm  pb-2 md:py-2
           md:w-3/5 lg:w-2/5 landscape:space-y-16"
           >
             <CopyBlock />
             <SocialProof count={waitlistQuery[0].count} />
             <WaitlistForm />
           </div>
-          {/* <MomSketchHero className="h-52 w-auto max-h-80 md:w-2/5 md:h-auto md:max-h-none landscape:min-h-48 " /> */}
           <Image 
-          src='/heroLine.png'
+          src='/MomSketchRealisticPlate.png'
           alt="Sketch of a mom handing a plate of zereshk polo ba morgh to her child."
           width={1024}
           height={1536}
-          className="h-52 w-auto max-h-80 md:w-2/5 md:h-auto md:max-h-none landscape:min-h-48 " 
+          className="h-72 w-auto max-h-80 md:w-2/5 md:h-auto md:max-h-none landscape:min-h-48 " 
           />
         </div>
       </MaxWidthWrapper>
