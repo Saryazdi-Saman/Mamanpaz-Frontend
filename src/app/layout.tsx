8769a085e77748dfc920d704bd6536ec2c52bed9
Saryazdi-Saman <saman.s90@gmail.com>
2025-07-11 20:14:42 -0400

    fix favicon issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata, Viewport } from 'next';
-import {ReactNode} from 'react';
+import type { Metadata, Viewport } from "next";
+import { ReactNode } from "react";
 
 export const viewport: Viewport = {
   themeColor: "#1D214E",
@@ -7,12 +7,36 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  viewportFit: "cover"
-}
+  viewportFit: "cover",
+};
 
 export const metadata: Metadata = {
-    metadataBase: new URL('https://www.mamanpazmeals.com'),
-}
+  metadataBase: new URL("https://www.mamanpazmeals.com"),
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: [
+      { url: "/icon1.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/web-app-manifest-192x192.png",
+  },
+  appleWebApp: {
+    title: "Mamanpaz",
+  },
+};
 
 type Props = {
   children: ReactNode;
@@ -20,6 +44,6 @@ type Props = {
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
+export default function RootLayout({ children }: Props) {
   return children;
 }
